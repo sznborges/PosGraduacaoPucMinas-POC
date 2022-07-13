a2ebfd8391612668efcc81bb8c275bf19861e2ee
Heverson <hresende.net@gmail.com>
2022-07-13 06:09:25 -0300

    Creating class User

---
+++ Gestor.Dashboard.Application/Contracts/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestor.Dashboard.Application.Contracts
+{
+    internal class User
+    {
+    }
+}
